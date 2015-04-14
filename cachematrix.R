aaf5b67035482e39c0855731c504445c53c0cdb7
Raul Bojalil <raulbojalil@gmail.com>
2015-04-14 18:56:20 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions compute and cache the inverse of square matrices.
 
-## Write a short comment describing this function
-
+## Creates an invertible matrix object from the specified square matrix.
+## Arguments:
+##    x  a square matrix.
+## Returns:
+##    An invertible matrix object.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    if(!identical(x,y))
+    {
+      x <<- y
+      m <<- NULL
+    }
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Computes and caches the inverse of the specified square matrix.
+## If the inverse matrix is already in cache it is returned immediately.
+## Otherwise, the inverse matrix is computed, cached and then returned.
+## Arguments:
+##    x   an invertible matrix object (which can be created using makeCacheMatrix)
+##    ... further arguments to pass to the underlying solve function call
+## Returns:
+##    A matrix (inverse matrix).
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+
+## --------------
+## Sample output:
+## --------------
+##
+## > cacheMatrix <- makeCacheMatrix(matrix(1:4,2,2))
+## > cacheSolve(cacheMatrix)
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(cacheMatrix)
+## getting cached data
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheMatrix$set(matrix(4:1,2,2))
+## > cacheSolve(cacheMatrix)
+##      [,1] [,2]
+## [1,] -0.5    1
+## [2,]  1.5   -2
+## > cacheSolve(cacheMatrix)
+## getting cached data
+##      [,1] [,2]
+## [1,] -0.5    1
+## [2,]  1.5   -2
